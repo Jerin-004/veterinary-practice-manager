5302d4d0f8a5f46a05aa8dbf5a745f9fa7234bfe
Chijith Jerin Stalin <chijithjerin@karunya.edu.in>
2026-02-07 15:08:52 +0530

    Update Emergency.jsx

--- frontend/src/Emergency.jsx
+++ frontend/src/Emergency.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
-const API = "https://veterinary-practice-manager.onrender.com/"
+const API = "https://veterinary-practice-manager.onrender.com"
 
 export default function Emergency() {
   const { petName } = useParams()
